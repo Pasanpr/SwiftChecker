b0ae3197be6688bb7e45c146bcaf99374174da48
Pasan Premaratne <pasan@premaratne.me>
2019-04-09 11:10:16 -0400

    Use remote dependency for SwiftAST

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
 		.library(name: "SwiftChecker", targets: ["SwiftChecker"]),
 	],
 	dependencies: [
-		.package(url: "/Users/pasan/Development/SwiftAST", from: "0.1.0"),
+		.package(url: "https://github.com/Pasanpr/SwiftAST", from: "0.1.0"),
 	],
 	targets: [
 		.target(name: "SwiftChecker", dependencies: ["SwiftAST"]),
